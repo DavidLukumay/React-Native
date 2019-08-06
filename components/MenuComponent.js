9a9c770c6c5b21fac52db283d4f2b0cb05ed3015
David Lukumay <lukumaydavid@gmail.com>
2019-08-06 03:02:47 +0300

    Components Part 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -7,13 +7,14 @@ function Menu(props) {
     const renderMenuItem = ({item, index}) => {
 
         return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    leftAvatar={{ source: require('./images/uthappizza.png')}}
-                  />
+            <ListItem
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron={true}
+                onPress={() => props.onPress(item.id)}
+                leftAvatar={{ source: require('./images/uthappizza.png')}}
+            />
         );
     };
 
@@ -22,7 +23,7 @@ function Menu(props) {
                 data={props.dishes}
                 renderItem={renderMenuItem}
                 keyExtractor={item => item.id.toString()}
-                />
+            />
     );
 }
 
