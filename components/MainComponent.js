b0cc642349abb620e902eec89143c479e2891bf4
David Lukumay <lukumaydavid@gmail.com>
2019-08-24 22:49:51 +0300

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import Dishdetail from './DishdetailComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 import { View, Platform,Image,StyleSheet,ScrollView,Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator,DrawerItems,SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -120,6 +121,23 @@ const ReservationNavigator = createStackNavigator({
         iconStyle={{ color: 'white' }} 
         onPress={ () => navigation.toggleDrawer()} />    
     })
+  });
+
+  const FavoritesNavigator = createStackNavigator({
+    Favorites: { screen: Favorites }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name="menu" size={24}
+        iconStyle={{ color: 'white' }} 
+        onPress={ () => navigation.navigate('DrawerToggle') } />    
+    })
   })
 
 
@@ -219,6 +237,22 @@ const MainNavigator = createDrawerNavigator({
             />
           ),
         }
+    },
+    
+    Favorites:
+    { screen: FavoritesNavigator,
+      navigationOptions: {
+        title: 'My Favorites',
+        drawerLabel: 'My Favorites',
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon
+            name='heart'
+            type='font-awesome'            
+            size={24}
+            iconStyle={{ color: tintColor }}
+          />
+        ),
+      }
     }
 },{
     drawerBackgroundColor: '#D1C4E9',
