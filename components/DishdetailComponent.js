55a965cb6a7ceecf299e0688075a5e38606d7f9d
David Lukumay <lukumaydavid@gmail.com>
2019-09-03 12:20:56 +0300

    Social Sharing

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card,Icon,Input,Rating } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -68,6 +68,16 @@ const mapStateToProps = state => {
               return true;
           }
       });
+
+      const shareDish = (title,message,url) => {
+          Share.share({
+              title: title,
+              message: title + ': ' + message + ' ' + url,
+              url: url,
+          },{
+            dialogTitle: 'Share ' + title
+          });
+      }
     
         if (dish != null) {
             return(
@@ -96,6 +106,15 @@ const mapStateToProps = state => {
                             type='font-awesome'
                             color='#512DA8'
                             onPress={() => openCommentForm()}
+                        />
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            style={styles.cardItem}
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} 
                         />
                     </View>
                     </Card>
